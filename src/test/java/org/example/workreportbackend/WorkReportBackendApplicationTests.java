c225c4933589387ead9f54b1f71bb0692dcf028e
Ludvig <ludvig.fendert@appliedtechnology.se>
2024-07-15 09:29:00 +0200

    inital

--- src/test/java/org/example/workreportbackend/WorkReportBackendApplicationTests.java
+++ src/test/java/org/example/workreportbackend/WorkReportBackendApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class WorkReportBackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
