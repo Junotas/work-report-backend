7570fbf74d6aa658c637f94da18766dafb940952
Ludvig <ludvig.fendert@appliedtechnology.se>
2024-07-15 09:28:48 +0200

    first commit

---
+++ src/main/java/org/example/workreportbackend/WorkReportBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.workreportbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkReportBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkReportBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/workreportbackend/WorkReportBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.workreportbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkReportBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
