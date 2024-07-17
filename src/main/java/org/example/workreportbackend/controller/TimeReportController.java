7c9bfd00842890a48424cceede00b92ee981fa9a
Ludvig <ludvig.fendert@appliedtechnology.se>
2024-07-17 07:22:36 +0200

    crossorigin update

--- src/main/java/org/example/workreportbackend/controller/TimeReportController.java
+++ src/main/java/org/example/workreportbackend/controller/TimeReportController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/time-reports")
-@CrossOrigin("http://localhost:5173")
+@CrossOrigin
 public class TimeReportController {
 
     private final TimeReportService timeReportService;
